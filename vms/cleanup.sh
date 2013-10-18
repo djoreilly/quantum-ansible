a733a28aea0087416ad9d5baa3003296ec56ba3a
Darragh O'Reilly <darragh.oreilly@server.fake>
2013-10-18 13:08:23 +0100

    Fix up smoketest for neutron cli

--- vms/cleanup.sh
+++ vms/cleanup.sh
@@ -7,25 +7,25 @@ nova delete vm1
 sleep 3
 
 
-for i in $(quantum floatingip-list -c id -f csv --quote none | tr -d '\r' | tail -n+2);
+for i in $(neutron floatingip-list -c id -f csv --quote none | tr -d '\r' | tail -n+2);
 do
-    quantum floatingip-delete $i
+    neutron floatingip-delete $i
 done
 
-quantum router-interface-delete router1 sub1
-quantum router-gateway-clear router1
-quantum router-delete router1
+neutron router-interface-delete router1 sub1
+neutron router-gateway-clear router1
+neutron router-delete router1
 
 
-for i in $(quantum port-list -c id -f csv --quote none | tr -d '\r' | tail -n+2);
+for i in $(neutron port-list -c id -f csv --quote none | tr -d '\r' | tail -n+2);
 do
-    quantum port-delete $i
+    neutron port-delete $i
 done
 
-quantum net-delete net1
-quantum net-delete ext-net
+neutron net-delete net1
+neutron net-delete ext-net
 
-quantum security-group-delete test-vms
+neutron security-group-delete test-vms
 
 echo "deleting key test-key"
 nova keypair-delete test-key
--- vms/run-sample-session.sh
+++ vms/run-sample-session.sh
@@ -10,25 +10,25 @@ nova flavor-create micro 6 50 0 1 || true
 chmod 0600 /vagrant/test-key
 nova keypair-add --pub-key /vagrant/test-key.pub test-key
 
-quantum net-create net1
-quantum subnet-create net1 10.0.33.0/24 --name=sub1 --dns_nameservers 8.8.4.4 8.8.8.8
+neutron net-create net1
+neutron subnet-create net1 10.0.33.0/24 --name=sub1 --dns_nameservers 8.8.4.4 8.8.8.8
 
-quantum net-create ext-net --provider:network_type local --router:external true
-quantum subnet-create ext-net 192.168.101.0/24 --enable_dhcp False --name ext-sub
+neutron net-create ext-net --provider:network_type local --router:external true
+neutron subnet-create ext-net 192.168.101.0/24 --enable_dhcp False --name ext-sub
 
-quantum router-create router1
-quantum router-gateway-set router1 ext-net
-quantum router-interface-add router1 sub1
+neutron router-create router1
+neutron router-gateway-set router1 ext-net
+neutron router-interface-add router1 sub1
 
 TEST_SG=test-vms
-quantum security-group-create $TEST_SG --description "allow ping and ssh"
-quantum security-group-rule-create --protocol icmp --direction ingress \
+neutron security-group-create $TEST_SG --description "allow ping and ssh"
+neutron security-group-rule-create --protocol icmp --direction ingress \
         --remote-ip-prefix 0.0.0.0/0 $TEST_SG
-quantum security-group-rule-create --protocol tcp --port-range-min 22 \
+neutron security-group-rule-create --protocol tcp --port-range-min 22 \
 	--port-range-max 22 --direction ingress --remote-ip-prefix 0.0.0.0/0 $TEST_SG
 
 
-PORT_ID=$(quantum port-create --security-group $TEST_SG net1 \
+PORT_ID=$(neutron port-create --security-group $TEST_SG net1 \
           -f shell --variable id | grep '^id=' | cut -d= -f2 | tr -d '"')
 
 sleep 5 # TODO find out why dhcp replies are dropped w/o this delay
@@ -37,11 +37,11 @@ nova boot --flavor micro --image cirros-0.3.1-x86_64 --nic port-id=$PORT_ID \
           --key-name test-key vm1
 
 
-FLOAT_ID=`quantum floatingip-create ext-net | grep ' id ' | awk '{print $4;}'`
+FLOAT_ID=`neutron floatingip-create ext-net | grep ' id ' | awk '{print $4;}'`
 VM_ID=`nova list | grep vm1 | awk '{print $2;}'`
 
-quantum floatingip-associate $FLOAT_ID $PORT_ID
-quantum floatingip-show $FLOAT_ID
+neutron floatingip-associate $FLOAT_ID $PORT_ID
+neutron floatingip-show $FLOAT_ID
 
-eval `quantum floatingip-show -f shell --variable floating_ip_address $FLOAT_ID`
+eval `neutron floatingip-show -f shell --variable floating_ip_address $FLOAT_ID`
 echo "Give VM a minute to boot before trying ssh -i vms/test-key cirros@${floating_ip_address} from main host"
